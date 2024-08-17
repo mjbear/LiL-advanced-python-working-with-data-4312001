fbab36a4f2691ed2dd9f9ce04a0abd53f9e4a109
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-08-17 21:01:45 +0000

    ch4 - basic logging (stdout or to file)

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,27 @@
 # demonstrate the logging api in Python
 
 # TODO: use the built-in logging module
-
+import logging
 
 # TODO: Use basicConfig to configure logging
+# basicConfig function is only executed once
+
+# outputs to stdout
+# logging.basicConfig(level=logging.DEBUG)
+
+# save to file (append is the default)
+# logging.basicConfig(level=logging.DEBUG, filename='output.log')
+# https://docs.python.org/3/howto/logging.html#logging-to-a-file
+logging.basicConfig(level=logging.DEBUG, filename='output.log', filemode='w')
 
 # TODO: Try out each of the log levels
+logging.debug('This is a debug message')
+logging.info('This is a info message')
+logging.warning('This is a warning message')
+logging.error('This is a error message')
+logging.critical('This is a critical message')
 
 # TODO: Output formatted strings to the log
-
+x = 'string'
+y = 10
+logging.info(f"Here's a {x} variable and an int: {y}")
