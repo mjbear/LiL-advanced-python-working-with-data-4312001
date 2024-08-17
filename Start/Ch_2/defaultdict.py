d0764da85f6fc436f43e43c72972924713c722d5
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-08-17 17:10:41 +0000

    ch2 - defaultdict type from collections

--- Start/Ch_2/defaultdict.py
+++ Start/Ch_2/defaultdict.py
@@ -8,10 +8,25 @@ fruits = ['apple', 'pear', 'orange', 'banana',
           'apple', 'grape', 'banana', 'banana']
 
 # TODO: use a dictionary to count each element
-fruitCounter = dict()
+# fruitCounter = dict()
+# fruitCounter = defaultdict(int)
+fruitCounter = defaultdict(lambda: 100)
 
 # TODO: Count the elements in the list
+# this will fail with a normal dictionary because the key does not exist
 for fruit in fruits:
     fruitCounter[fruit] += 1
 
+# lots of unnecessary code while using a normal dictionary
+# for fruit in fruits:
+#     if fruit in fruitCounter.keys():
+#         fruitCounter[fruit] += 1
+#     else:
+#         fruitCounter[fruit] = 1
+
 # TODO: print the result
+print(fruitCounter)
+print()
+
+for k, v in fruitCounter.items():
+    print(f'{k}: {v}')
