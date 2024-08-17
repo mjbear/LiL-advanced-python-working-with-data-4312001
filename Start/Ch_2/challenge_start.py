a0efb5b095e3bcc9e186c5524ed7baa1d37fdd50
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-08-17 17:52:48 +0000

    ch2 - challenge - earthquake type quantities

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,23 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
+from collections import defaultdict
 
 
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+    # categorize each event and count each one
+    type_count = defaultdict(int)
+    for event in data['features']:
+        type_count[event['properties']['type']] += 1
+
+    # return the result
+    print(type_count)
+    print()
+    print(dict(type_count))
+
+    print()
+    for k, v in type_count.items():
+        print(f'{k:15}: {v}')
