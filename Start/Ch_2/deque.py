8803e7cddf53bc64aea69a57f45b2880f0c23869
Michael Bear <38406045+mjbear@users.noreply.github.com>
2024-08-17 17:36:59 +0000

    ch2 - deque Class

--- Start/Ch_2/deque.py
+++ Start/Ch_2/deque.py
@@ -1,15 +1,31 @@
 # deque objects are like double-ended queues
+# (pronounced "deck")
 
 import collections
 import string
 
 
 # TODO: initialize a deque with lowercase letters
+d = collections.deque(string.ascii_lowercase)
 
 # TODO: deques support the len() function
+print(f'Item Count: {len(d)}')
 
 # TODO: deques can be iterated over
+for elem in d:
+    print(elem.upper())
 
 # TODO: manipulate items from either end
+d.pop()
+d.popleft()
+d.append(2)
+d.appendleft(1)
+print(d)
 
 # TODO: use an index to get a particular item
+d.rotate(1)
+print(d)
+
+print()
+d.rotate(5)
+print(d)
